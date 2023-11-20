0bc662c9fefe3b19d5c6d7921d0c9a0965d08a13
David Ostos <davidostos2@gmail.com>
2023-11-20 14:04:11 -0400

    creando repo de proyecto 03-pokedex

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+
+    // coneccion a base de datos
+    MongooseModule.forRoot(process.env.MONGODB),
+
+    PokemonModule,
+
+    CommonModule,
+
+    SeedModule
+  ]
+})
+export class AppModule {
+
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor () {
+    // console.log( process.env );
+  }
+}
---
+++ src/config/env.config.ts
@@ -0,0 +1,18 @@
+
+
+export const EnvConfiguration = () => ({
+  enviroment: process.env.NODE_ENV || 'dev',
+  mongodb: process.env.MONGODB,
+  port: process.env.PORT || 3001,
+  defaultLimit: process.env.DEFAULT_LIMIT || 7
+});
+
+
+
+/* esta es la equivalencia a la arrow function de EnvConfiguration
+const envfb = () => {
+  return {
+
+  };
+};
+ */
